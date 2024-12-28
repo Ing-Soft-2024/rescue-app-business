a1d2ace2097810f782a4909e0fd8c9bb167e3aae
juane993 <jolivamorroni@itba.edu.ar>
2024-12-28 20:41:54 -0300

    fixed navigation drawer incorrect screens appearing

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -5,9 +5,6 @@ import { MaterialIcons } from "@expo/vector-icons";
 import { Redirect, useRouter } from "expo-router";
 import { Drawer } from "expo-router/drawer";
 import { Pressable } from "react-native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-
-
 
 export default function AppLayout() {
     const { session, signOut } = useSession();
@@ -17,71 +14,67 @@ export default function AppLayout() {
 
         
     return (
-        <GestureHandlerRootView style={{
-            flex: 1,
-            backgroundColor: '#121212',
-        }}>
-            <Drawer
-                screenOptions={{
-                    // headerBackTitleVisible: false,
-                    headerTintColor: '#D4685E',
-                    headerRight: () => (
-                        <Pressable
-                            style={({ pressed }) => ({
-                                padding: 10,
-                                opacity: pressed ? 0.5 : 1
-                            })}
-                            onPress={() => router.push("/notifications")}
-                        >
-                            <MaterialIcons name="notifications" size={24} color={"#D4685E"} />
-                        </Pressable>
-                    ),
-                    drawerInactiveTintColor: '#E1958E',
-                    drawerActiveTintColor: '#D4685E',
-                    drawerType: "front",
-                }}
-                drawerContent={(props) => <DrawerContent {...props} />}
-            >
-                <Drawer.Screen
-                    name="index"
-                    options={{
-                        title: "Mis Productos",
-                        drawerIcon: (props) => <MaterialIcons name="home" {...props} />,
-                        headerRight: () => <NotificationsBell />
-                    }} />
-                <Drawer.Screen
-                    name="notifications/index"
-                    options={{
-                        title: "Notificaciones",
-                        drawerIcon: (props) => <MaterialIcons name="notifications" {...props} />
-                    }} />
-                <Drawer.Screen
-                    name="scan/index"
-                    options={{
-                        title: "Escanear",
-                        "headerShown": false,
-                        drawerIcon: (props) => <MaterialIcons name="qr-code-scanner" {...props} />,
-                    }} />
+        <Drawer
+            screenOptions={{
+                headerTintColor: '#D4685E',
+                headerRight: () => (
+                    <Pressable
+                        style={({ pressed }) => ({
+                            padding: 10,
+                            opacity: pressed ? 0.5 : 1
+                        })}
+                        onPress={() => router.push("/notifications")}
+                    >
+                        <MaterialIcons name="notifications" size={24} color={"#D4685E"} />
+                    </Pressable>
+                ),
+                drawerInactiveTintColor: '#E1958E',
+                drawerActiveTintColor: '#D4685E',
+                drawerType: "front",
+            }}
+            drawerContent={(props) => <DrawerContent {...props} />}
+        >
+            <Drawer.Screen
+                name="index"
+                options={{
+                    title: "Mis Productos",
+                    drawerIcon: (props) => <MaterialIcons name="home" {...props} />,
+                    headerRight: () => <NotificationsBell />
+                }} />
+            <Drawer.Screen
+                name="notifications/index"
+                options={{
+                    title: "Notificaciones",
+                    drawerIcon: (props) => <MaterialIcons name="notifications" {...props} />
+                }} />
+            <Drawer.Screen
+                name="scan/index"
+                options={{
+                    title: "Escanear",
+                    
+                    drawerIcon: (props) => <MaterialIcons name="qr-code-scanner" {...props} />,
+                }} />
 
-                <Drawer.Screen
-                    name="scan/scannedOrder"
-                    options={{
-                        title: "Escanear",
-                        drawerIcon: (props) => <MaterialIcons name="qr-code-scanner" {...props} />,
-                        "swipeEnabled": false,
-                        "headerLeft": () => null,
-                        "headerRight": () => null,
-                    }} />
-                <Drawer.Screen
-                    name="product"
-                    options={{
-                        "title": "Agregar Producto",
-                        "headerShown": false,
-                        "drawerIcon": (props) => <MaterialIcons name="add" {...props} />,
-                        "swipeEnabled": false,
-                    }}
-                />
-            </Drawer>
-        </GestureHandlerRootView>
+            <Drawer.Screen
+                name="scan/scannedOrder"
+                options={{
+                    title: "Escanear",
+                    drawerIcon: (props) => <MaterialIcons name="qr-code-scanner" {...props} />,
+                    drawerItemStyle: { display: 'none' },
+                    swipeEnabled: false,
+                    headerLeft: () => null,
+                    headerRight: () => null,
+                }} /> 
+            <Drawer.Screen
+                name="product"
+                options={{
+                    "title": "Agregar Producto",
+                    "headerShown": false,
+                    "drawerIcon": (props) => <MaterialIcons name="add" {...props} />,
+                    "drawerItemStyle": { display: 'none' },
+                    "swipeEnabled": false,
+                }}
+            />
+        </Drawer>
     );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,18 @@ import { BusinessProvider } from '../src/context/business.context';
 import { NotificationsProvider } from '../src/context/notifications.context';
 import { SessionProvider } from '../src/context/session.context';
 import { Slot } from "expo-router";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
   return (
-    <SessionProvider>
-      <BusinessProvider>
-        <NotificationsProvider>
-          <Slot />
-        </NotificationsProvider>
-      </BusinessProvider>
-    </SessionProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SessionProvider>
+        <BusinessProvider>
+          <NotificationsProvider>
+            <Slot />
+          </NotificationsProvider>
+        </BusinessProvider>
+      </SessionProvider>
+    </GestureHandlerRootView>
   );
 }
