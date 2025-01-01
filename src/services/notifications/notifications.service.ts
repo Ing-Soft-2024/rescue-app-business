0b2b88d4d05d0fb020f38bea6d75d3d3088b7df7
juane993 <jolivamorroni@itba.edu.ar>
2025-01-01 19:55:15 -0300

    .

--- src/services/notifications/notifications.service.ts
+++ src/services/notifications/notifications.service.ts
@@ -1,5 +1,8 @@
+import { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform } from 'react-native';
+import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
-import { Platform } from 'react-native';
+import Constants from 'expo-constants';
 
 // Configure notifications behavior for both foreground and background
 Notifications.setNotificationHandler({
@@ -45,6 +48,74 @@ export class NotificationsService {
 
         return true;
     }
+
+    static async sendPushNotification(expoPushToken: string) {
+        const message = {
+            to: expoPushToken,
+            sound: 'default',
+            title: 'Original Title',
+            body: 'And here is the body!',
+            data: { someData: 'goes here' },
+        };
+      
+        await fetch('https://exp.host/--/api/v2/push/send', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Accept-encoding': 'gzip, deflate',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(message),
+        });
+    }
+    static handleRegistrationError(errorMessage: string) {
+        alert(errorMessage);
+        throw new Error(errorMessage);
+      }
+
+
+    static async registerForPushNotificationsAsync() {
+        if (Platform.OS === 'android') {
+          Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+          });
+        }
+      
+        if (Device.isDevice) {
+          const { status: existingStatus } = await Notifications.getPermissionsAsync();
+          let finalStatus = existingStatus;
+          if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+          }
+          if (finalStatus !== 'granted') {
+            NotificationsService.handleRegistrationError('Permission not granted to get push token for push notification!');
+            return;
+          }
+          const projectId =
+            Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+          if (!projectId) {
+            NotificationsService.handleRegistrationError('Project ID not found');
+          }
+          try {
+            const pushTokenString = (
+              await Notifications.getExpoPushTokenAsync({
+                projectId,
+              })
+            ).data;
+            console.log(pushTokenString);
+            return pushTokenString;
+          } catch (e: unknown) {
+            NotificationsService.handleRegistrationError(`${e}`);
+          }
+        } else {
+          NotificationsService.handleRegistrationError('Must use physical device for push notifications');
+        }
+      }
+      
 
     static async scheduleNotification(title: string, body: string) {
         try {
